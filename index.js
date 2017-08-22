5799910588856ac8b1da46a1ebc58c5aec7c2d31
Sebastian Chinowski <github@sebastianchinowski.pl>
2017-08-22 15:28:35 +0200

    Add full screen comparing with firefox

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const width = 1920;
 //const height = 1080;
 
 var driver = new webdriver.Builder()
-    .forBrowser('chrome')
+    .forBrowser('firefox')
     .build();
 
 webdriver.WebDriver.prototype.saveScreenshot = function(filename) {
@@ -25,27 +25,29 @@ function processScrenshoots(driver, name) {
             curr: `screenshots/${name}_current.png`,
             prev: `screenshots/${name}_previous.png`
         };
-        driver.saveScreenshot(file.curr).then(function() {
-            if (fs.existsSync(file.prev)) {
-                imageDiff.getFullResult({
-                    actualImage: file.curr,
-                    expectedImage: file.prev,
-                    diffImage: `screenshots/${name}_difference.png`,
-                    shadow: true
-                }, function(err, result) {
-                    fs.unlinkSync(file.prev);
-                    fs.renameSync(file.curr, file.prev);
+        setTimeout(() => {
+            driver.saveScreenshot(file.curr).then(function() {
+                if (fs.existsSync(file.prev)) {
+                    imageDiff.getFullResult({
+                        actualImage: file.curr,
+                        expectedImage: file.prev,
+                        diffImage: `screenshots/${name}_difference.png`,
+                        shadow: true
+                    }, function(err, result) {
+                        fs.unlinkSync(file.prev);
+                        fs.renameSync(file.curr, file.prev);
 
-                    if (err) return reject(err);
-                    return resolve(result);
-                });
-            } else {
-                fs.renameSync(file.curr, file.prev);
-                return resolve({
-                    message: 'No previous file'
-                });
-            }
-        });
+                        if (err) return reject(err);
+                        return resolve(result);
+                    });
+                } else {
+                    fs.renameSync(file.curr, file.prev);
+                    return resolve({
+                        message: 'No previous file'
+                    });
+                }
+            });
+        }, 5000);
     });
 }
 /*
@@ -61,28 +63,29 @@ elements: [
  */
 
 describe('Homepage', () => {
-    it('Should open homepage', function(done) {
+    it('should open homepage', function(done) {
         this.timeout(0);
         driver.get('https://networkrail.co.uk/').then((err, data) => {
             setTimeout(() => {
-                done(err);
-            }, 3000);
+                done();
+            }, 500);
         });
     });
-    it('Should resize window', (done) => {
+    it('should resize window', (done) => {
         driver.findElement(By.xpath('//html')).then((el) => {
             driver.executeScript(function() {
                 return document.getElementsByTagName('body')[0].scrollHeight;
             }).then(function(height) {
-                console.log(height);
-                driver.manage().window().setSize(width, height).then(done);
+                //console.log(height);
+                driver.manage().window().setSize(width, height);
+                done();
             });
         });
     });
-    it('Should have less difference than 10%', function(done) {
+    it('should have less difference than 10%', function(done) {
         this.timeout(0);
         processScrenshoots(driver, 'homepage').then((result) => {
-            console.log(result);
+            //console.log(result);
             if (result.message) {
                 console.log(result.message);
                 return done(null);
@@ -90,12 +93,13 @@ describe('Homepage', () => {
             try {
                 assert.equal(result.percentage * 100 < 10, true, Math.floor(result.percentage * 100, 2) + '%');
             } catch (e) {
+                //console.log('e: ', e);
                 return done(e);
             }
             done();
         }).catch(done);
     });
-    it('Should close the browser', (done) => {
+    it('should close the browser', (done) => {
         driver.quit().then((err) => {
             done(err);
         });
